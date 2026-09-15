bc59534e51bba0a613dae15332027342d97898ae
MattChampagne <1bizzeedad@gmail.com>
2026-09-15 15:09:14 -0500

    V1.55 enable invoice workflow

--- app/owner/layout.js
+++ app/owner/layout.js
@@ -1,6 +1,7 @@
 import './owner.css';
 import RescheduleEnhancer from './RescheduleEnhancer';
+import InvoiceEnhancer from './InvoiceEnhancer';
 
 export default function OwnerLayout({children}){
-  return <>{children}<RescheduleEnhancer/></>;
+  return <>{children}<RescheduleEnhancer/><InvoiceEnhancer/></>;
 }
