023dd0817326058ff2a0197881083e957ad51170
MattChampagne <1bizzeedad@gmail.com>
2026-09-16 07:14:49 -0500

    V1.58 enable paid invoice save counter

--- app/owner/layout.js
+++ app/owner/layout.js
@@ -1,7 +1,8 @@
 import './owner.css';
 import RescheduleEnhancer from './RescheduleEnhancer';
 import InvoiceEnhancer from './InvoiceEnhancer';
+import PaidInvoiceCounter from './PaidInvoiceCounter';
 
 export default function OwnerLayout({children}){
-  return <>{children}<RescheduleEnhancer/><InvoiceEnhancer/></>;
+  return <>{children}<RescheduleEnhancer/><InvoiceEnhancer/><PaidInvoiceCounter/></>;
 }
