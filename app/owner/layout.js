0784071d9e51004f7fe31e81c97430b4842387b3
MattChampagne <1bizzeedad@gmail.com>
2026-09-17 15:55:33 -0500

    V1.77 load Job Readiness

--- app/owner/layout.js
+++ app/owner/layout.js
@@ -4,7 +4,8 @@ import InvoiceEnhancer from './InvoiceEnhancer';
 import PaidInvoiceCounter from './PaidInvoiceCounter';
 import DailyOverview from './DailyOverview';
 import DailyChecklist from './DailyChecklist';
+import JobReadiness from './JobReadiness';
 
 export default function OwnerLayout({children}){
-  return <>{children}<DailyOverview/><DailyChecklist/><RescheduleEnhancer/><InvoiceEnhancer/><PaidInvoiceCounter/></>;
+  return <>{children}<DailyOverview/><DailyChecklist/><JobReadiness/><RescheduleEnhancer/><InvoiceEnhancer/><PaidInvoiceCounter/></>;
 }
