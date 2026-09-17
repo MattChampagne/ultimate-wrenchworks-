2244015107e2ff4a7d1bc5d6a8b6e934a6e87f76
MattChampagne <1bizzeedad@gmail.com>
2026-09-17 15:37:19 -0500

    V1.74 load Owner Daily Overview

--- app/owner/layout.js
+++ app/owner/layout.js
@@ -2,7 +2,8 @@ import './owner.css';
 import RescheduleEnhancer from './RescheduleEnhancer';
 import InvoiceEnhancer from './InvoiceEnhancer';
 import PaidInvoiceCounter from './PaidInvoiceCounter';
+import DailyOverview from './DailyOverview';
 
 export default function OwnerLayout({children}){
-  return <>{children}<RescheduleEnhancer/><InvoiceEnhancer/><PaidInvoiceCounter/></>;
+  return <>{children}<DailyOverview/><RescheduleEnhancer/><InvoiceEnhancer/><PaidInvoiceCounter/></>;
 }
